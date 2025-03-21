94c946d9050be27017ced93c9ac4d8ddcd742f9b
Beedlebub <beedlebub@gmail.com>
2025-03-21 09:23:24 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    strip.showRainbow(1, 360)
-    wuKong.setAllMotor(-100, -100)
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, 100)
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, -100)
     basic.pause(2000)
     wuKong.stopAllMotor()
 })
@@ -8,12 +8,11 @@ input.onButtonPressed(Button.AB, function () {
     wuKong.stopAllMotor()
 })
 input.onButtonPressed(Button.B, function () {
-    wuKong.setAllMotor(100, 100)
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, -100)
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, 100)
     basic.pause(2000)
     wuKong.stopAllMotor()
 })
-let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
 basic.forever(function () {
 	
 })
